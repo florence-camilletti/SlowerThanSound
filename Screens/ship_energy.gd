8d4dfe155be3ca196efdb97b7bd9a74ae8c6386c
Florence Camilletti <flocamilletti@gmail.com>
2025-04-16 18:18:15 -0500

    Curr sprite commit

---
+++ Screens/ship_energy.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var menu_text
+var ship_img_path = "res://Assets/Texts/Ship_Energy.txt"
+
+var heading_pwr: int
+var oxygen_pwr: int
+var AI_pwr: int
+var weapon_pwr: int
+var bulk_pwr: int
+var target_pwr: int
+var eng_lft_pwr: int
+var eng_mid_pwr: int
+var eng_rht_pwr: int
