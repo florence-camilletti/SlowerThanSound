df7990269e93934ac3de27e5770f56f17efaa435
Florence Camilletti <flocamilletti@gmail.com>
2025-06-13 21:34:27 -0500

    Test LIDAR added

--- Scripts/enemy_manager.gd
+++ Scripts/enemy_manager.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var rng = RandomNumberGenerator.new()
 var num_enemies: int
 var max_enemies = 1
 var enemy_list: Array
@@ -11,4 +12,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     if(num_enemies < max_enemies):
         print("MAKING NEW ENEMY")
-        num_enemies+=1
+        make_new_enemy()
+
+func make_new_enemy() -> void:
+    num_enemies+=1
+    var rnd_x = rng.randf_range(-1, 1)
+    var rnd_y = rng.randf_range(-1, 1)
+    var enemy_pos = Vector2(rnd_x, rnd_y)
+    #TODO: MAKE AN ENEMY OBJECT
+    enemy_list.append(enemy_pos)
+    print("ENEMY AT "+str(enemy_pos))
+    
+func get_pos() -> Vector2:
+    return(enemy_list[0])
