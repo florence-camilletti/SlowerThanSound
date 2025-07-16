09f47b8c1d09cf08182d89f4ee4292a677f411eb
Florence Camilletti <flocamilletti@gmail.com>
2025-07-15 22:53:40 -0500

    Minor typos

--- Scripts/ship_manager.gd
+++ Scripts/ship_manager.gd
@@ -63,7 +63,6 @@ func _process(delta: float):
     self.knot_speed+=delta_knot_speed
     self.sub_position+=knot_to_dectic(self.heading, self.knot_speed)
     self.LIDAR_child.update_sub_pos(self.sub_position, self.heading)
-    #print(translate_speed(self.heading, self.speed))
     
 func _input(event):
     for possible_action in range(num_actions):#Check for menu change
