056486dd05c0676705107853f9ef502203a50e49
Florence Camilletti <flocamilletti@gmail.com>
2025-07-15 22:45:50 -0500

    Made player rotate on LIDAR

--- Scripts/ship_manager.gd
+++ Scripts/ship_manager.gd
@@ -62,7 +62,7 @@ func _process(delta: float):
     self.depth+=delta_depth
     self.knot_speed+=delta_knot_speed
     self.sub_position+=knot_to_dectic(self.heading, self.knot_speed)
-    self.LIDAR_child.update_sub_pos(self.sub_position)
+    self.LIDAR_child.update_sub_pos(self.sub_position, self.heading)
     #print(translate_speed(self.heading, self.speed))
     
 func _input(event):
