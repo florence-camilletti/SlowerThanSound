44710cf6fb7c0dae832eb95be8b83140b6bbbcf0
Florence Camilletti <flocamilletti@gmail.com>
2025-07-21 02:34:31 -0500

    torp base scaffold added

---
+++ Scripts/Torps/torp_base.gd
@@ -0,0 +1,14 @@
+extends Node2D
+class_name TorpBase
+
+func _init() -> void:
+    pass
+    
+func _ready() -> void:
+    pass
+    
+func _process(delta: float) -> void:
+    pass
+    
+func _to_string() -> String:
+    return("TEMP")
